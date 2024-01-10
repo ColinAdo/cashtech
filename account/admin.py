70cae4a3d5df219823203a122a44092d2fbf5319
ColinAdo <colinadodev30@gmail.com>
2024-01-10 19:50:28 +0300

    Register account model to admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Account
+
+class AccountAdmin(admin.ModelAdmin):
+    list_editable = ['account_status', 'account_balance']
+    list_filter = ['account_status']
+    list_display = ['user', 'account_status' ,'account_number', 'account_balance']
+
+
+admin.site.register(Account, AccountAdmin)
