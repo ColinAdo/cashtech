872a107a5bf780e37f0cb39c6f9538762c29d6a0
ColinAdo <colinadodev30@gmail.com>
2024-01-11 22:16:43 +0300

    Registered KYC model to admin

--- account/admin.py
+++ account/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from .models import Account
+from .models import Account, KYC
 
 class AccountAdmin(admin.ModelAdmin):
     list_editable = ['account_status', 'account_balance']
     list_filter = ['account_status']
     list_display = ['user', 'account_status' ,'account_number', 'account_balance']
 
+class KYCAdmin(admin.ModelAdmin):
+    list_display = ['user', 'full_name', 'country', 'dob']
+    search_fields = ['full_name']
+
 
 admin.site.register(Account, AccountAdmin)
+admin.site.register(KYC, KYCAdmin)
