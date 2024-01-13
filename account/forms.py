65a2db2699438a42f6902a89ae3bff8b484e57f6
ColinAdo <colinadodev30@gmail.com>
2024-01-13 13:27:33 +0300

    Add forms.py

---
+++ account/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from django.forms import ImageField, FileInput, DateInput
+
+from.models import KYC
+
+class DateInput(forms.DateInput):
+    input_type = 'date' # Allow user to select date and time
+
+class KYCForm(forms.ModelForm):
+    identity_image = ImageField(widget=FileInput)
+    image = ImageField(widget=FileInput)
+    signiture = ImageField(widget=FileInput)
+
+    class Meta:
+        model = KYC
+        fields = [
+            'full_name',
+            'image',
+            'gender',
+            'identity_type',
+            'identity_image',
+            'marital_status',
+            'signature',
+            'dob',
+            'country',
+            'state',
+            'city',
+            'mobile',
+            'fax',
+        ]
+        widgets = {
+            'full_name': forms.TextInput(attrs={'placeholder':'Full Name'}),
+            'country': forms.TextInput(attrs={'placeholder': 'Country'}),
+            'state': forms.TextInput(attrs={'placeholder':'State'}),
+            'city': forms.TextInput(attrs={'placeholder':'City'}),
+            'mobile': forms.TextInput(attrs={'placeholder':'Mobile'}),
+            'dob': DateInput,
+        }
+
