5e1daba765f7ae28d270d4b9ce434fd591ae1919
ColinAdo <colinadodev30@gmail.com>
2024-01-13 14:36:29 +0300

    Add view for handling kyc form

--- account/views.py
+++ account/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+from .models import KYC
+from .forms import KYCForm
+
+def kyc_registration(request):
+    template = 'account/kyc_reg_form.html'
+    user = request.user
+    try:
+        kyc = KYC.objects.get(user=user)
+    except:
+        kyc = None
+
+    if request.method == 'POST':
+        form = KYCForm(request.POST, request.FILES, instance=kyc)
+        if form.is_valid():
+            new_kyc = form.save()
+            new_kyc.user = user
+            new_kyc.save()
+
+            messages.success(request, "Kyc saved successfully")
+            return redirect('index')
+    else:
+        form = KYCForm(instance=kyc)
+
+    context = {
+        'form': form,
+        'kyc': kyc
+    }
+    return render(request,template, context)
+
+
+
