cbeb32f80b9bf2378f36d7e95d2bae32738c2f19
ColinAdo <colinadodev30@gmail.com>
2024-01-18 15:59:39 +0300

    Add dashboard url and template files

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,6 @@ from .import views
 
 urlpatterns = [
     path('', views.account, name='account'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('kyc-reg/', views.kyc_registration, name='kyc'),
 ]
