2048650af0eddc16c943d92b34a80167db1e6645
ColinAdo <colinadodev30@gmail.com>
2024-01-18 18:26:21 +0300

    Add request confirm  to the url

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path("request/", request_account.request_account, name="request_account"),
     path("request-amount/<account_number>/",request_account.request_ammount, name="request_amount"),
     path("request-transaction/<account_number>/",request_account.request_transaction, name="request_transaction"),
+    path("request-confirm/<account_number>/<transaction_id>/",request_account.request_confirm, name="request_confirm"),
 
 ]
