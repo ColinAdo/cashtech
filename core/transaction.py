ac4f8d2144f88cf4246e8a59a04282f3b12a712e
ColinAdo <colinadodev30@gmail.com>
2024-01-19 16:27:14 +0300

    Updated filter to get transfer transaction only

--- core/transaction.py
+++ core/transaction.py
@@ -3,8 +3,8 @@ from .models import Transaction
 
 def transaction_list(request):
     template = 'transaction/transaction_list.html'
-    sender_transaction = Transaction.objects.filter(sender=request.user)
-    receiver_transaction = Transaction.objects.filter(reciever=request.user)
+    sender_transaction = Transaction.objects.filter(sender=request.user, transaction_type='transfer')
+    receiver_transaction = Transaction.objects.filter(reciever=request.user, transaction_type='transfer')
 
     context = {
         'sender_transaction': sender_transaction,
