c431f22aac81916705afd9a4009ae5de560d527d
ColinAdo <colinadodev30@gmail.com>
2024-01-20 15:45:14 +0300

    Add credit card form

---
+++ core/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+
+from .models import CreditCard
+
+class CreditCardForm(forms.ModelForm):
+    
+    class Meta:
+        model = CreditCard
+        fields = [
+            'name',
+            'amount',
+            'number',
+            'month',
+            'year',
+            'cvv',
+            'credit_type',
+        ]
+
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Card Name'}),
+            'amount': forms.TextInput(attrs={'placeholder': 'Amount'}),
+            'number': forms.TextInput(attrs={'placeholder': 'Card Number'}),
+            'month': forms.TextInput(attrs={'placeholder': 'Expiry Month'}),
+            'year': forms.TextInput(attrs={'placeholder': 'Expiry Year'}),
+            'cvv': forms.TextInput(attrs={'placeholder': 'Card Cvv'}),
+        }
