287d6b00dd2e2c502815dfcf60476fa311f3606e
ColinAdo <colinadodev30@gmail.com>
2024-01-22 13:39:06 +0300

    Update settings urls.py

--- cashtech/urls.py
+++ cashtech/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('account/', include('account.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
